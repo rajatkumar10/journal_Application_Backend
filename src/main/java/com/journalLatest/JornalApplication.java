feb403465758f8125ea6eaea65cd432cb2659ab3
rajat kumar <rajatko614@gmail.com>
2026-05-16 12:46:02 +0530

    [ADD]: First Commit

---
+++ src/main/java/com/journalLatest/JornalApplication.java
@@ -0,0 +1,13 @@
+package com.journalLatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JornalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JornalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/journalLatest/JornalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.journalLatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JornalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
